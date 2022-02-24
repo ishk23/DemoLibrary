d832223b22ca43caf57f4d1b66c0bc5746ce0fc4
IshikaK <ishika.khandelwal2306@gmail.com>
2022-02-24 23:26:22 +0530

    Added rest of CRUD functionality in BookController

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -7,17 +7,11 @@ namespace Library.Controllers
 {
     public class BookController : Controller, IBook
     {
-
         private readonly ApplicationDbContext _db;
 
         public BookController(ApplicationDbContext db)
         {
             _db = db;
-        }
-        // GET: Book
-        public ActionResult Index()
-        {
-            return View();
         }
 
         // POST: Book/Create
@@ -34,6 +28,65 @@ namespace Library.Controllers
             catch
             {
                 return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetBooks()
+        {
+            try
+            {
+                var books = _db.Books.ToList();
+                return Json(books);
+            }
+            catch
+            {
+                return Json(new { IsProcessDone = false, ProcessMessage = "Failure :(" });
+            }
+
+        }
+        [HttpGet]
+        public ActionResult FindById(int id)
+        {
+            try
+            {
+                var book = _db.Books.Find(id);
+                return Json(book);
+            }
+            catch
+            {
+                return Json(new { IsProcessDone = false, ProcessMessage = "Failure :(" });
+            }
+
+        }
+        [HttpDelete]
+        public ActionResult DeleteBook(int id)
+        {
+            var book = _db.Books.Find(id);
+            if (book != null)
+            {
+                _db.Books.Remove(book);
+                _db.SaveChanges();
+                return Json(new { IsProcessDone = true, ProcessMessage = "Success!" });
+            }
+            else
+            {
+                return Json(new { IsProcessDone = false, ProcessMessage = "Failure :(" });
+            }
+        }
+
+        [HttpPut]
+        public ActionResult UpdateBook(Book book)
+        {
+            try
+            {
+                _ = _db.Books.Update(book);
+                _db.SaveChanges();
+                return Json(new { IsProcessDone = true, ProcessMessage = "Success!" });
+            }
+            catch
+            {
+                return Json(new { IsProcessDone = false, ProcessMessage = "Failure :(" });
             }
         }
 
